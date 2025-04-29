a3e32d973aee389ecbf0bb10f26fe35e796629c4
Jules MUNTZ-BERGER <julmuntz@student.42.fr>
2025-04-29 19:40:23 +0200

    ex14: changeable FOV

--- src/14.cpp
+++ src/14.cpp
@@ -6,14 +6,19 @@ int main()
 {
     try
     {
+        float fov;
+        std::cout << "Enter FOV angle in degrees: ";
+        std::cin >> fov;
+
         std::ofstream file("matrix_display/proj");
         if (file.is_open())
-            std::cout << "Enter these commands:" << std::endl
-                      << std::endl
+        {
+            file << projection(fov, 1, 0.01f, 10);
+            std::cout << "\nMatrix generated with FOV = " << fov << " degrees" << std::endl;
+            std::cout << "\nEnter these commands to view the matrix:" << std::endl
                       << "-\tcd matrix_display/" << std::endl
-                      << "-\t./display" << std::endl
-                      << std::endl;
-        file << projection(45, 1, 0.01f, 10);
+                      << "-\t./display" << std::endl;
+        }
     }
     catch (const std::exception &e)
     {
